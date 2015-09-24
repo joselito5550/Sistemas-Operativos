408ea47e5fb1707cfac919043282dd84bfde7520
joselito5550 <i42marmj@uco.es>
2015-09-24 10:45:00 +0200

    Escritura en ficheros

---
+++ P1/ejer5.c
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char **argv){
+    pid_t pid;
+    int status, childpid,i;
+    FILE *f = fopen(argv[1],"w");
+
+    pid=fork();
+    if(pid==0){
+         for(i=0;i<10;i++){
+               fprintf(f," %s", "++++++++++++\n");
+               sleep(1);
+         }
+         fclose(f);
+         exit(EXIT_SUCCESS);
+   }
+   else {
+         for(i=0;i<10;i++){
+               fprintf(f," %s", "---------------------\n");
+               sleep(1);
+         }
+   }
+   wait(&status);
+   fclose(f);
+    exit(EXIT_SUCCESS);
+}
