6e13de754c33e08d1033403997cba32a5b0ad18e
joselito5550 <i42marmj@uco.es>
2015-10-01 08:57:49 +0200

    Actualizadas demos

--- P1/Demos/demo9_bis.c
+++ P1/Demos/demo9_bis.c
@@ -3,28 +3,33 @@
 #include	<signal.h>
 #include	<stdio.h>
 #include	<unistd.h>
+#include <stdlib.h>
+#include <errno.h>
 
 static void sig_alrm(int signo)
 {
-	printf("entre en la funcion\n");
+	printf("In the sig_alrm function!!!\n");
+	printf("Value of signo:%d\n",signo);
 	return;	/* nothing to do, just return to wake up the pause */
 }
 
-unsigned int sleep1(unsigned int nsecs)
+unsigned int f1(unsigned int nsecs)
 {
+	if (signal(SIGALRM, sig_alrm) == SIG_ERR)
+	{
+		perror("Signal error");
+   	printf("errno value= %d\n", errno);  
+		exit(EXIT_FAILURE);
+	}
 	alarm(nsecs);		/* start the timer */
-	pause();			/* next caught signal wakes us up */
+	pause();			/* pause - suspend the thread until a signal is received */ 
 }
-
-#include <stdlib.h>
-#include <unistd.h>
 
 int main ()
 {
-	signal(SIGALRM, sig_alrm);
     printf ("Una alarma en 3 segundos.\n");
-    sleep1(3);
+    f1(3);
     printf ("Terminado el primer sleep...\n");
-    sleep1(2);
+    f1(2);
     printf ("Terminado el segundo sleep...\n");
 }
